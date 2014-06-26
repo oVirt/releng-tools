84e82b145ab6d2f773ad79aa40f0df1ef3cb8122
Martin Perina <mperina@redhat.com>
2014-06-26 19:57:22 +0200

    kexec-tools-el6: Version 2.0.0-273.1 with fence_kdump patches
    Applies fence_kdump patches to kexec-tools-2.0.0-273 (part of RHEL 6.5)
    so package can be provided by oVirt repository until RHEL 6.6 is
    released (which will contain kexec-tools-2.0.0-274 with fence_kdump
    patches).
    
    Change-Id: Ie555465cd7cd39c6e6fa5d9c65733823c12ddc4b
    Signed-off-by: Martin Perina <mperina@redhat.com>

---
+++ specs/kexec-tools-el6/build.sh
@@ -0,0 +1,30 @@
+#!/bin/sh -e
+
+src="kexec-tools-2.0.0-273.el6.src.rpm"
+url="http://vault.centos.org/6.5/os/Source/SPackages/${src}"
+patches="0001-Add-function-is_pcs_fence_kdump.patch \
+         0002-Add-function-get_pcs_cluster_nodes.patch \
+         0003-Add-function-setup_cluster_nodes_and_options.patch \
+         0004-Add-fence_kdump-support-for-generic-clusters.patch \
+         0005-Update-spec.patch"
+
+# Download the source RPM and extract it
+if [ ! -f "${src}" ]
+then
+    wget -O "${src}" "${url}"
+fi
+rpm2cpio ${src} | cpio -idmv
+
+# Apply patches for fence_kdump support
+for p in ${patches}; do
+  patch -p1 < ${p}
+done
+
+
+# Build the source and binary packages:
+rpmbuild \
+    -bs \
+    --define="_sourcedir ${PWD}" \
+    --define="_srcrpmdir ${PWD}" \
+    --define="_rpmdir ${PWD}" \
+    "kexec-tools.spec"
