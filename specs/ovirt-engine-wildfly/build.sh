77bb58154964a770695a85a7b89fe8a345001474
Juan Hernandez <juan.hernandez@redhat.com>
2014-07-17 14:03:15 +0200

    Add spec file for WildFly 8
    This patch adds a build script and spec file that can be used to
    download and repackage the .zip distribution of WildFly 8 as a RPM
    package.
    
    Change-Id: Ib835a3575f8404ad1e22f8877dd6ad2f60de4d47
    Bug-Url: https://bugzilla.redhat.com/1121327
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>

---
+++ specs/ovirt-engine-wildfly/build.sh
@@ -0,0 +1,28 @@
+#!/bin/sh -e
+
+# The name, version and source of the package:
+name="ovirt-engine-wildfly"
+version="8.1.0"
+src="wildfly-${version}.Final.zip"
+url="http://download.jboss.org/wildfly/${version}.Final/${src}"
+
+# Download the source:
+if [ ! -f "${src}" ]
+then
+    wget -O "${src}" "${url}"
+fi
+
+# Generate the spec from the template:
+sed \
+    -e "s/@VERSION@/${version}/g" \
+    -e "s/@SRC@/${src}/g" \
+    < "${name}.spec.in" \
+    > "${name}.spec"
+
+# Build the source and binary packages:
+rpmbuild \
+    -bs \
+    --define="_sourcedir ${PWD}" \
+    --define="_srcrpmdir ${PWD}" \
+    --define="_rpmdir ${PWD}" \
+    "${name}.spec"
