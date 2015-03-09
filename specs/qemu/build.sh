973b028c2f41d0b6355aa718d948b21c68c40644
Sandro Bonazzola <sbonazzo@redhat.com>
2015-03-09 13:50:12 +0100

    build: qemu-kvm-ev for el7
    Re-build qemu-kvm-rhev removing the branding and
    renaming the final rpm to qemu-kvm-ev.
    
    Also fixes a dependency error on libcacard-tools-ev
    causing conflicts between qemu-img and qemu-img-ev.
    
    Change-Id: I654aa97c414548361db8c9f8d8e271819062b4ed
    Bug-Url: https://bugzilla.redhat.com/1199014
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

---
+++ specs/qemu/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -e
+
+src="qemu-kvm-rhev-2.1.2-23.el7.src.rpm"
+url="ftp://ftp.redhat.com/pub/redhat/linux/enterprise/7Server/en/RHOS/SRPMS/${src}"
+patches=(
+    "0001-qemu-kvm-spec-remove-branding.patch"
+    "0002-fix-qemu-img-require.patch"
+)
+# Download the source RPM and extract it
+if ! [[ -f "${src}.orig" ]]
+then
+    wget -O "${src}.orig" "${url}"
+fi
+rpm2cpio "${src}.orig" | cpio -idmv
+
+# Apply patches for qemu re-branding
+for patchset in "${patches[@]}" ; do
+  patch -p1 < "${patchset}"
+done
+
+
+# Build the source and binary packages:
+rpmbuild \
+    -bs \
+    --define="_sourcedir ${PWD}" \
+    --define="_srcrpmdir ${PWD}" \
+    --define="_rpmdir ${PWD}" \
+    "qemu-kvm.spec"
