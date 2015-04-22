2a2c65e63ee7ae1e9aa643b30376a00a310c2690
Sandro Bonazzola <sbonazzo@redhat.com>
2015-04-22 09:10:30 +0200

    virtsig checker fixes
    - Check new packages only in epel7 tag
    - Include only completed builds
    
    Change-Id: Ie3a1fe4da4690c42b2f7a5160bf1c2fa19b098ad
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- check_virtsig_need_update.py
+++ check_virtsig_need_update.py
@@ -24,7 +24,7 @@ import koji
 
 CENTOS_SERVER = 'https://cbs.centos.org/kojihub/'
 FEDORA_SERVER = 'http://koji.fedoraproject.org/kojihub/'
-FEDORA_STABLE = 'f22'
+FEDORA_STABLE = 'epel7' #f22, epel7
 
 centos_session = koji.ClientSession(CENTOS_SERVER)
 fedora_session = koji.ClientSession(FEDORA_SERVER)
@@ -82,20 +82,23 @@ for package in tag_packages:
                 if 'build_id' in x and x['tag.name'] == FEDORA_STABLE:
                     build_info = fedora_session.getBuild(x['build_id'])
                     if build_info is not None:
-                        task_id = build_info['task_id']
-                        task = fedora_session.getTaskInfo(
-                            task_id,
-                            request=True,
-                        )
-                        task_state = task['state']
-                        if task_state == koji.TASK_STATES['CLOSED']:
-                            # If CLOSED is true then the package finished
-                            # building successfully
-                            task_request = task['request']
-                            task_method = task['method']
-                            fedora_package_version = x['version']
-                            fedora_package_release = x['release']
-                            break
+                        if (
+                            build_info['state'] == koji.BUILD_STATES['COMPLETE']
+                        ):
+                            task_id = build_info['task_id']
+                            task = fedora_session.getTaskInfo(
+                                task_id,
+                                request=True,
+                            )
+                            task_state = task['state']
+                            if task_state == koji.TASK_STATES['CLOSED']:
+                                # If CLOSED is true then the package finished
+                                # building successfully
+                                task_request = task['request']
+                                task_method = task['method']
+                                fedora_package_version = x['version']
+                                fedora_package_release = x['release']
+                                break
     except koji.GenericError as e:
         if not ignore_missing:
             print(e)
