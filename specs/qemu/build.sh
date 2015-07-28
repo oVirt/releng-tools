a1f25c80aad10d7352f546d37e4115677e3d7342
Sandro Bonazzola <sbonazzo@redhat.com>
2015-07-28 11:37:38 +0200

    build: qemu-kvm-rhev-2.1.2-23.el7_1.6
    Change-Id: Id6d3560b4394cfd5613f4992f2571bd86dc6d4f8
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- specs/qemu/build.sh
+++ specs/qemu/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-src="qemu-kvm-rhev-2.1.2-23.el7_1.4.src.rpm"
+src="qemu-kvm-rhev-2.1.2-23.el7_1.6.src.rpm"
 url="ftp://ftp.redhat.com/pub/redhat/linux/enterprise/7Server/en/RHEV/SRPMS/${src}"
 patches=(
     "0001-qemu-kvm-spec-remove-branding.patch"
