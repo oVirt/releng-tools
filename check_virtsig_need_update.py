e8b24f3ba291f9a1cf825833053130f8b891d03a
Sandro Bonazzola <sbonazzo@redhat.com>
2015-07-28 10:56:15 +0200

    check_virtsig_need_update: sync with cbs tags
    Change-Id: I0dd439ad1676fb22aecc6ec46ea0cccfe55c26d6
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- check_virtsig_need_update.py
+++ check_virtsig_need_update.py
@@ -22,115 +22,181 @@
 import koji
 
 
-CENTOS_SERVER = 'https://cbs.centos.org/kojihub/'
+CENTOS_SERVER = 'http://cbs.centos.org/kojihub/'
 FEDORA_SERVER = 'http://koji.fedoraproject.org/kojihub/'
-FEDORA_STABLE = 'epel7' #f22, epel7
+FEDORA_STABLE = 'f22'
+EPEL_STABLE = 'epel7'
 
 centos_session = koji.ClientSession(CENTOS_SERVER)
 fedora_session = koji.ClientSession(FEDORA_SERVER)
-verbose = False
-ignore_missing = True
+verbose = True
+ignore_missing = False
 
-user_tables = centos_session.queryHistory(
-    user='sbonazzo',
-    tag_name='virt7-testing',
-)
-tag_packages = user_tables['tag_packages']
 
-for package in tag_packages:
-    if package['revoke_event'] is not None:
-        continue
-    package_name = package['package.name']
-    centos_package_version = ''
-    centos_package_release = ''
-    fedora_package_version = ''
-    fedora_package_release = ''
-    try:
-        centos_tables = centos_session.queryHistory(package=package_name)
-        for table in centos_tables:
-            build = centos_tables[table]
-            for x in build:
-                if 'build_id' in x and x['tag.name'] == 'virt7-testing':
-                    build_info = centos_session.getBuild(x['build_id'])
-                    if build_info is not None:
-                        task_id = build_info['task_id']
-                        task = centos_session.getTaskInfo(
-                            task_id,
-                            request=True
-                        )
-                        task_state = task['state']
-                        if task_state == koji.TASK_STATES['CLOSED']:
-                            # If CLOSED is true then the package finished
-                            # building successfully
-                            task_request = task['request']
-                            task_method = task['method']
-                            centos_package_version = x['version']
-                            centos_package_release = x['release']
-                            break
-    except koji.GenericError as e:
-        if not ignore_missing:
-            print(e)
-    try:
-        fedora_tables = fedora_session.queryHistory(
-            package=package_name,
-            tag_name=FEDORA_STABLE,
-            task_state=koji.TASK_STATES['CLOSED'],
-        )
-        for table in fedora_tables:
-            build = fedora_tables[table]
-            for x in build:
-                if 'build_id' in x and x['tag.name'] == FEDORA_STABLE:
-                    build_info = fedora_session.getBuild(x['build_id'])
-                    if build_info is not None:
-                        if (
-                            build_info['state'] == koji.BUILD_STATES['COMPLETE']
-                        ):
+def check_cbs_tag(cbs_tag):
+    if verbose:
+        print('checking %s' % cbs_tag)
+    user_tables = centos_session.queryHistory(
+        user='sbonazzo',
+        tag_names=cbs_tag,
+    )
+    tag_packages = user_tables['tag_packages']
+
+    for package in tag_packages:
+        if package['revoke_event'] is not None:
+            continue
+        package_name = package['package.name']
+        centos_package_version = ''
+        centos_package_release = ''
+        epel_package_version = ''
+        epel_package_release = ''
+        fedora_package_version = ''
+        fedora_package_release = ''
+        try:
+            centos_tables = centos_session.queryHistory(package=package_name)
+            for table in centos_tables:
+                build = centos_tables[table]
+                for x in build:
+                    if 'build_id' in x and x['tag.name'] == cbs_tag:
+                        build_info = centos_session.getBuild(x['build_id'])
+                        if build_info is not None:
                             task_id = build_info['task_id']
-                            task = fedora_session.getTaskInfo(
+                            task = centos_session.getTaskInfo(
                                 task_id,
-                                request=True,
+                                request=True
                             )
                             task_state = task['state']
                             if task_state == koji.TASK_STATES['CLOSED']:
                                 # If CLOSED is true then the package finished
                                 # building successfully
-                                task_request = task['request']
-                                task_method = task['method']
-                                fedora_package_version = x['version']
-                                fedora_package_release = x['release']
+                                centos_package_version = x['version']
+                                centos_package_release = x['release']
                                 break
-    except koji.GenericError as e:
-        if not ignore_missing:
-            print(e)
-    fedora_nvr = (
-        str(fedora_package_version) +
-        '-' +
-        str(fedora_package_release.split('.')[0])
-    )
-    centos_nvr = (
-        str(centos_package_version) +
-        '-' +
-        str(centos_package_release.split('.')[0])
-    )
-    # if fedora_nvr > centos_nvr:
-    # Let's ignore rpm releases for now... We've already enough to do.
-    if str(fedora_package_version) > str(centos_package_version):
-        print(
-            '%s need an update from %s to %s' % (
-                package_name,
-                centos_nvr,
-                fedora_nvr
+        except koji.GenericError as e:
+            if not ignore_missing:
+                print(e)
+        try:
+            epel_tables = fedora_session.queryHistory(
+                package=package_name,
+                tag_name=EPEL_STABLE,
+                task_state=koji.TASK_STATES['CLOSED'],
             )
+            for table in epel_tables:
+                build = epel_tables[table]
+                for x in build:
+                    if 'build_id' in x and x['tag.name'] == EPEL_STABLE:
+                        build_info = fedora_session.getBuild(x['build_id'])
+                        if build_info is not None:
+                            if (
+                                build_info['state'] ==
+                                koji.BUILD_STATES['COMPLETE']
+                            ):
+                                task_id = build_info['task_id']
+                                task = fedora_session.getTaskInfo(
+                                    task_id,
+                                    request=True,
+                                )
+                                task_state = task['state']
+                                if task_state == koji.TASK_STATES['CLOSED']:
+                                    # If CLOSED is true then the package
+                                    # finished building successfully
+                                    epel_package_version = x['version']
+                                    epel_package_release = x['release']
+                                    break
+        except koji.GenericError as e:
+            if not ignore_missing:
+                print(e)
+        try:
+            fedora_tables = fedora_session.queryHistory(
+                package=package_name,
+                tag_name=FEDORA_STABLE,
+                task_state=koji.TASK_STATES['CLOSED'],
+            )
+            for table in fedora_tables:
+                build = fedora_tables[table]
+                for x in build:
+                    if 'build_id' in x and x['tag.name'] == FEDORA_STABLE:
+                        build_info = fedora_session.getBuild(x['build_id'])
+                        if build_info is not None:
+                            if (
+                                build_info['state'] ==
+                                koji.BUILD_STATES['COMPLETE']
+                            ):
+                                task_id = build_info['task_id']
+                                task = fedora_session.getTaskInfo(
+                                    task_id,
+                                    request=True,
+                                )
+                                task_state = task['state']
+                                if task_state == koji.TASK_STATES['CLOSED']:
+                                    # If CLOSED is true then the package
+                                    # finished building successfully
+                                    fedora_package_version = x['version']
+                                    fedora_package_release = x['release']
+                                    break
+        except koji.GenericError as e:
+            if not ignore_missing:
+                print(e)
+        epel_nvr = (
+            str(epel_package_version) +
+            '-' +
+            str(epel_package_release.split('.')[0])
         )
-    else:
-        if not ignore_missing:
-            if fedora_nvr == '-':
-                print('%s is missing in fedora' % package_name)
-            if centos_nvr == '-':
-                print('%s is missing in centos' % package_name)
-        if verbose and fedora_nvr != '-' and centos_nvr != '-':
+        fedora_nvr = (
+            str(fedora_package_version) +
+            '-' +
+            str(fedora_package_release.split('.')[0])
+        )
+        centos_nvr = (
+            str(centos_package_version) +
+            '-' +
+            str(centos_package_release.split('.')[0])
+        )
+        # if fedora_nvr > centos_nvr:
+        # Let's ignore rpm releases for now... We've already enough to do.
+        if (str(fedora_package_version) > str(centos_package_version)):
             print(
-                '%s is ok, we have %s and %s is available' % (
-                    package_name, centos_nvr, fedora_nvr
+                '%s may need an update from %s to fedora %s within %s' % (
+                    package_name,
+                    centos_nvr,
+                    fedora_nvr,
+                    cbs_tag
                 )
             )
+        if (str(epel_package_version) > str(centos_package_version)):
+            print(
+                '%s need an update from %s to epel %s within %s' % (
+                    package_name,
+                    centos_nvr,
+                    epel_nvr,
+                    cbs_tag
+                )
+            )
+        else:
+            if not ignore_missing:
+                if fedora_nvr == '-':
+                    print('%s is missing in fedora' % package_name)
+                if epel_nvr == '-':
+                    print('%s is missing in epel' % package_name)
+                if centos_nvr == '-':
+                    print('%s is missing in centos' % package_name)
+            if verbose and epel_nvr != '-' and centos_nvr != '-':
+                print(
+                    '%s is ok, we have %s and %s is available\n' % (
+                        package_name, centos_nvr, epel_nvr
+                    )
+                )
+            elif verbose and fedora_nvr != '-' and centos_nvr != '-':
+                print(
+                    '%s is ok, we have %s and %s is available\n' % (
+                        package_name, centos_nvr, fedora_nvr
+                    )
+                )
+
+for tag_to_check in (
+    'virt7-ovirt-common-candidate',
+    'virt7-ovirt-36-candidate',
+    'virt7-ovirt-35-candidate',
+    'virt7-kvm-common-candidate',
+):
+    check_cbs_tag(tag_to_check)
