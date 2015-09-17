3a4d62a5c1cec188fec8c9533efbef92a1f6aa25
Sandro Bonazzola <sbonazzo@redhat.com>
2015-09-17 13:25:10 +0200

    Update WildFly to 10.0.0
    This patch updates the spec file for the WildFly RPM to upstream version
    10.0.0.
    
    Change-Id: I27eb213b0bb14511d69048a332fc63609c8fc78a
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

---
+++ specs/ovirt-engine-wildfly10/build.sh
@@ -0,0 +1,30 @@
+#!/bin/sh -e
+
+# The name, version and source of the package:
+name="ovirt-engine-wildfly"
+version="10.0.0"
+qualifier="CR1"
+src="wildfly-${version}.${qualifier}.zip"
+url="http://download.jboss.org/wildfly/${version}.${qualifier}/${src}"
+
+# Download the source:
+if [ ! -f "${src}" ]
+then
+    wget -O "${src}" "${url}"
+fi
+
+# Generate the spec from the template:
+sed \
+    -e "s/@VERSION@/${version}/g" \
+    -e "s/@QUALIFIER@/${qualifier}/g" \
+    -e "s/@SRC@/${src}/g" \
+    < "${name}.spec.in" \
+    > "${name}.spec"
+
+# Build the source and binary packages:
+rpmbuild \
+    -bs \
+    --define="_sourcedir ${PWD}" \
+    --define="_srcrpmdir ${PWD}" \
+    --define="_rpmdir ${PWD}" \
+    "${name}.spec"
