b6f7f9f3422049b3d20abe1c9cb7f2f4430fd312
Sandro Bonazzola <sbonazzo@redhat.com>
2015-12-11 15:46:14 +0100

    debian: ioprocess: rebase on 0.15.0-4
    - Rebase on 0.15.0-4
    - Re-enable automatic testing
    - Require autoreconf since github tar doesn't include configure.
    - Added myself as uploader
    
    Change-Id: Ie60da3d8c49f32e381404cd3075e161724bf9bba
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- debian/ioprocess/build.sh
+++ debian/ioprocess/build.sh
@@ -2,18 +2,21 @@
 
 # The name, version and source of the package:
 name="ioprocess"
-version="0.15.0"
+version="0.15.0.4"
+tag="0.15.0-4"
 release="1"
 archive="${name}_${version}.orig.tar.gz"
-url="http://resources.ovirt.org/pub/src/${name}/${name}-${version}.tar.gz"
+url="https://github.com/oVirt/${name}/archive/v${tag}.tar.gz"
+# url="http://resources.ovirt.org/pub/src/${name}/${name}-${version}.tar.gz"
 
 
 # Download the source:
 wget -O "${archive}" "${url}"
 tar xzf "${archive}"
-cp -r debian "${name}-${version}/"
+rm -rf "${name}-${tag}/debian"
+cp -r debian "${name}-${tag}/"
 
-cd "${name}-${version}"
+cd "${name}-${tag}"
 dpkg-source -b .
 
 sudo -E /usr/sbin/pbuilder build --hookdir ../pbuilder-hooks --buildresult ../ ../*.dsc
