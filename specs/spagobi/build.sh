b5c5c3ca57bcd943cff465f4e7e0ab17b3a276b3
Sandro Bonazzola <sbonazzo@redhat.com>
2016-02-18 16:10:35 +0100

    specs: spagobi: minimal build from branch
    - Switched from trunk to 5.2 stabilization branch
    - Downloading and including in the tarball only what
      is needed to build. Reducing jenkins build time and
      artifacts size.
    
    Change-Id: If3927d68393ea3a24db5553373033e8fbc0a29d6
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- specs/spagobi/build.sh
+++ specs/spagobi/build.sh
@@ -6,13 +6,66 @@
 
 
 [ -d trunk ] && rm -rf trunk
-URL="svn://svn.forge.objectweb.org/svnroot/spagobi/V_5.x/Server/trunk"
+URL="svn://svn.forge.objectweb.org/svnroot/spagobi/V_5.x/Server/branches/SpagoBI-5.2"
 SVNREV=$(svn info --xml ${URL}|xpath "string(//entry/@revision)" 2>/dev/null)
 
-svn export -r ${SVNREV} ${URL}
+DIRS=(
+#     cas
+#     SpagoBIBirtReportEngine
+    spagobi-core
+#     SpagoBIJPivotEngine
+    SpagoBIOAuth2SecurityProvider
+#     SpagoBITalendEngineCilentAPI
+#     ChefCookbooks
+#     SpagoBIChartEngine
+    SpagoBIDAO
+    SpagoBILdapSecurityProvider
+    SpagoBIUtils
+    QbeCore
+#     SpagoBICockpitEngine
+    SpagoBIDatabaseScripts
+    SpagoBILiferaySecurityProvider
+    SpagoBIProject
+    SpagoBIUtilsJSON
+#     resources
+#     SpagoBICommonJEngine
+#     SpagoBIDataMiningEngine
+    spagobi-metamodel-core
+    SpagoBIQbeEngine
+#     SpagoBIWhatIfEngine
+#     Servers
+    spagobi-commons-core
+#     SpagoBIGeoEngine
+    spagobi-metamodel-utils
+#     SpagoBISDK
+#     SpagoBIXXXSpagoEngine
+#     SpagoBIAccessibilityEngine
+#     SpagoBIComponent
+#     SpagoBIGeoReportEngine
+#     SpagoBIMobileEngine
+#     SpagoBISocialAnalysis
+#     spagobi.birt.oda
+#     SpagoBIConsoleEngine
+    SpagoBIJasperReportEngine
+#     SpagoBINetworkEngine
+#     SpagoBITalendEngine
+)
+
+mkdir trunk
+svn export -r ${SVNREV} ${URL}/spagobi-parent trunk/spagobi-parent
 
 VERSION=$(xpath trunk/spagobi-parent/pom.xml  "//project/version/text()" 2>/dev/null)
 CHECKOUT="$(date -u +%Y%m%d)svn${SVNREV}"
+
+echo "Building version ${VERSION}"
+
+for project in "${DIRS[@]}"
+do
+    echo "Exporting ${project}"
+    svn --quiet export -r ${SVNREV} ${URL}/${project} trunk/${project}
+done
+
+
 
 [ -f spagobi5x-${CHECKOUT}.tar.xz ] && rm -f spagobi5x-${CHECKOUT}.tar.xz
 [ -d spagobi5x-${CHECKOUT} ] && rm -rf spagobi5x-${CHECKOUT}
