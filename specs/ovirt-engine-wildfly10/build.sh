fedfc561161f535c069aaf50d6a8510f2796a74e
Martin Perina <mperina@redhat.com>
2016-02-22 10:25:58 +0100

    ovirt-engine-wildfly10: Upgrade to 10.0.0.Final
    Upgrade to WildFly 10.0.0.CR5
    
    Change-Id: I150fc47a0f6c18846b72a631f77a47b374172edd
    Signed-off-by: Martin Perina <mperina@redhat.com>

--- specs/ovirt-engine-wildfly10/build.sh
+++ specs/ovirt-engine-wildfly10/build.sh
@@ -3,7 +3,7 @@
 # The name, version and source of the package:
 name="ovirt-engine-wildfly"
 version="10.0.0"
-qualifier="CR5"
+qualifier="Final"
 src="wildfly-${version}.${qualifier}.zip"
 url="http://download.jboss.org/wildfly/${version}.${qualifier}/${src}"
 
