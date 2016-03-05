db28cfbef1220107a2a81e2614c60533b21ee04c
Juan Hernandez <juan.hernandez@redhat.com>
2016-03-05 21:50:30 +0100

    Added Node.js packages
    This patch adds two .spec files needed to build Node.js and the NPM
    modules that will be used by oVirt JavaScript applications like the
    new dashboard that will be introduced in version 4.0 of the engine.
    
    The first package "ovirt-engine-nodejs" is just a repackaging of the
    Node.js binaries.
    
    The second package "ovirt-engine-nodejs-modules" is the set of NPM
    modules that will be needed to build the oVirt JavaScript applications.
    At the moment the set of modules is composed of "less" and its
    dependencies, and it is just an example.
    
    Change-Id: I52d7680f9d6d9dc313640776311d59febb599ac3
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>

---
+++ specs/ovirt-engine-nodejs-modules/build.sh
@@ -0,0 +1,44 @@
+#!/bin/sh -ex
+
+# The name, version and source of the package:
+name="ovirt-engine-nodejs-modules"
+
+# Download the node binaries and extract them to a local directory:
+node_version="4.3.2"
+node_dir="node-v${node_version}-linux-x64"
+node_tar="${node_dir}.tar.xz"
+node_url="https://nodejs.org/dist/v${node_version}/${node_tar}"
+if [ ! -f "${node_tar}" ]
+then
+    wget -O "${node_tar}" "${node_url}"
+fi
+rm -rf "${node_dir}"
+tar -xf "${node_tar}"
+
+# Configure the path environment variable so that the node binaries
+# installed in the previous step are always used before any other node
+# installation that may be available in the system:
+export PATH="${PWD}/${node_dir}/bin:${PATH}"
+
+# Clean the local modules directory, run "npm" so that it will
+# download all the dependencies listed in the "package.json" file and
+# create a tar file containing everything:
+modules_dir="node_modules"
+modules_tar="${modules_dir}.tar.xz"
+rm -rf "${modules_dir}"
+npm install
+tar -cJf "${modules_tar}" -C "${modules_dir}" .
+
+# Generate the spec from the template:
+sed \
+    -e "s/@TAR@/${modules_tar}/g" \
+    < "${name}.spec.in" \
+    > "${name}.spec"
+
+# Build the source and binary packages:
+rpmbuild \
+    -ba \
+    --define="_sourcedir ${PWD}" \
+    --define="_srcrpmdir ${PWD}" \
+    --define="_rpmdir ${PWD}" \
+    "${name}.spec"
---
+++ specs/ovirt-engine-nodejs/build.sh
@@ -0,0 +1,29 @@
+#!/bin/sh -e
+
+# The name, version and source of the package:
+name="ovirt-engine-nodejs"
+version="4.3.2"
+src="node-v${version}-linux-x64.tar.xz"
+url="https://nodejs.org/dist/v${version}/node-v${version}-linux-x64.tar.xz"
+
+# Download the source:
+if [ ! -f "${src}" ]
+then
+    wget -O "${src}" "${url}"
+fi
+
+# Generate the spec from the template:
+sed \
+    -e "s/@VERSION@/${version}/g" \
+    -e "s/@QUALIFIER@/${qualifier}/g" \
+    -e "s/@SRC@/${src}/g" \
+    < "${name}.spec.in" \
+    > "${name}.spec"
+
+# Build the source and binary packages:
+rpmbuild \
+    -ba \
+    --define="_sourcedir ${PWD}" \
+    --define="_srcrpmdir ${PWD}" \
+    --define="_rpmdir ${PWD}" \
+    "${name}.spec"
