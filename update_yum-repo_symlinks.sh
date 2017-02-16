3b67ab980c27acf2305d1f338872efdcc82226f6
Sandro Bonazzola <sbonazzo@redhat.com>
2017-02-16 10:10:15 +0100

    update_yum-repo_symlinks: added 4.0 snapshot
    Change-Id: I6b5f87e1dc3611998dc69b2f61a024a58b099061
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- update_yum-repo_symlinks.sh
+++ update_yum-repo_symlinks.sh
@@ -1,10 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 export MASTER="/var/www/html/pub/ovirt-master-snapshot/rpm/el7/noarch"
 export OVIRT41="/var/www/html/pub/ovirt-4.1-snapshot/rpm/el7/noarch"
+export OVIRT40="/var/www/html/pub/ovirt-4.0-snapshot/rpm/el7/noarch"
 export YUMREPO="/var/www/html/pub/yum-repo"
 
 ln -sf `ls "${MASTER}"/ovirt-release-master-4* |tail -n 1` "${YUMREPO}/ovirt-release-master.rpm"
 ln -sf `ls "${MASTER}"/ovirt-release-master-experimental-4* |tail -n 1` "${YUMREPO}/ovirt-release-master-experimental.rpm"
 ln -sf `ls "${OVIRT41}"/ovirt-release41-snapshot-4* |tail -n 1` "${YUMREPO}/ovirt-release41-snapshot.rpm"
 ln -sf `ls "${OVIRT41}"/ovirt-release41-experimental-4* |tail -n 1` "${YUMREPO}/ovirt-release41-experimental.rpm"
+ln -sf `ls "${OVIRT40}"/ovirt-release40-snapshot-4* |tail -n 1` "${YUMREPO}/ovirt-release40-snapshot.rpm"
