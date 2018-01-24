ca328efaf5530a34486d8563d14b14ebb04dbf9c
Sandro Bonazzola <sbonazzo@redhat.com>
2018-01-24 09:29:32 +0100

    update_yum-repo_symlinks: add 4.2 snapshot
    Change-Id: Ic57a5fb13edbba21d5566ef732cab4047f826be2
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- update_yum-repo_symlinks.sh
+++ update_yum-repo_symlinks.sh
@@ -2,8 +2,10 @@
 
 export MASTER="/var/www/html/pub/ovirt-master-snapshot/rpm/el7/noarch"
 export OVIRT41="/var/www/html/pub/ovirt-4.1-snapshot/rpm/el7/noarch"
+export OVIRT42="/var/www/html/pub/ovirt-4.2-snapshot/rpm/el7/noarch"
 export YUMREPO="/var/www/html/pub/yum-repo"
 
 ln -sf `ls "${MASTER}"/ovirt-release-master-4* |tail -n 1` "${YUMREPO}/ovirt-release-master.rpm"
 ln -sf `ls "${MASTER}"/ovirt-release-master-experimental-4* |tail -n 1` "${YUMREPO}/ovirt-release-master-experimental.rpm"
 ln -sf `ls "${OVIRT41}"/ovirt-release41-snapshot-4* |tail -n 1` "${YUMREPO}/ovirt-release41-snapshot.rpm"
+ln -sf `ls "${OVIRT42}"/ovirt-release42-snapshot-4* |tail -n 1` "${YUMREPO}/ovirt-release42-snapshot.rpm"
