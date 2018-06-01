4364074647872fb701c339a49fd99e420c5946e1
Sandro Bonazzola <sbonazzo@redhat.com>
2018-06-01 14:07:56 +0200

    automation: add automation
    - Test release conf files by running repoman on them
    - Test milestone conf files by generating release notes on them
    
    Change-Id: I83fd5b9e6c3b29914e944be3a0765a5559f4b913
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

---
+++ automation/check-patch.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -xe
+[[ -d exported-artifacts ]] || mkdir -p exported-artifacts
+
+if git show --pretty="format:" --name-only | egrep -q "^releases/.*conf$"; then
+    for config_file in `git show --pretty="format:" --name-only | egrep "^releases/.*conf$"`
+    do
+        repoman exported-artifacts add conf:"${config_file}"
+    done
+    if [[ -n `find exported-artifacts -name "*.git*.rpm"` ]] ; then
+        echo "RPMs with git hash in NVR are not allowed within releases" >&2
+        exit 1
+    fi
+fi
+
+
+if git show --pretty="format:" --name-only | egrep -q "^milestones/.*conf$"; then
+    for config_file in `git show --pretty="format:" --name-only | egrep "^milestones/.*conf$"`
+    do
+        release=`basename "${config_file/.conf/}"`
+        ./release_notes_git.py "${release}" > "exported-artifacts/${release}_notes.md"
+    done
+fi
