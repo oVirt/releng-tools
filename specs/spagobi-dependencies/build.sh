0d22d03575eae283ad411e1e9b5612a61747b505
Sandro Bonazzola <sbonazzo@redhat.com>
2018-10-23 17:17:31 +0200

    specs: drop spagobi files
    we are not working on spagobi and there are no plans
    to work on it in the future, dropping
    
    Change-Id: Ia8a7165bddbfd5d058eaf7580c7f6d0d65ca39a4
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- specs/spagobi-dependencies/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-make dist
-yum-builddep spagobi-dependencies.spec
-rpmbuild \
-    -bs \
-    --define="_sourcedir ${PWD}" \
-    --define="_srcrpmdir ${PWD}" \
-    --define="_rpmdir ${PWD}" \
-    "spagobi-dependencies.spec"
