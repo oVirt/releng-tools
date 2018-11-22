e2a7a6001eb98ac988dbfe942d5c272f90ec08e4
Sandro Bonazzola <sbonazzo@redhat.com>
2018-11-22 16:32:43 +0100

    4.3.0_beta1: add more packages
    Added:
    - ovirt-ansible-hosted-engine-setup
    - ovirt-ansible-vm-infra
    - ovirt-ansible-cluster-upgrade
    - ovirt-ansible-v2v-conversion-host
    - ovirt-ansible-engine-setup
    - ovirt-ansible-repositories
    - ovirt-ansible-disaster-recovery 1.1.3
    - ovirt-engine-api-explorer
    - ovirt-ansible-image-template
    - ovirt-dwh
    - ovirt-engine-cli
    - ovirt-engine-metrics
    - engine-db-query
    - java-ovirt-engine-sdk4
    - ovirt-ansible-manageiq
    - ovirt-ansible-shutdown-env
    - ovirt-engine-extension-aaa-ldap
    - ovirt-engine-extension-aaa-misc
    
    Changed also test script to not archive the collected repo
    in order to speed up tests.
    
    Change-Id: I2fef616d1107ab6be6cdca07a1f3871b156b95b5
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>
    Signed-off-by: Martin Perina <mperina@redhat.com>

--- automation/check-patch.sh
+++ automation/check-patch.sh
@@ -1,14 +1,17 @@
 #!/bin/bash -xe
 [[ -d exported-artifacts ]] || mkdir -p exported-artifacts
+[[ -d compose-test ]] || mkdir -p compose-test
 
 if git show --pretty="format:" --name-only | egrep -q "^releases/.*conf$"; then
     for config_file in `git show --pretty="format:" --name-only | egrep "^releases/.*conf$"`
     do
         if [[ -e "${config_file}" ]] ; then
-            repoman exported-artifacts add conf:"${config_file}"
             if [[ "${config_file}" =~ "alpha" ]] || [[ "${config_file}" =~ "beta" ]]; then
+                # Not archiving composed repository to save time during 4.3 mass import
+                repoman compose-test add conf:"${config_file}"
                 echo "Skipping git hash test being pre-release compose"
             else
+                repoman exported-artifacts add conf:"${config_file}"
                 if [[ -n `find exported-artifacts -name "*.git*.rpm"` ]] ; then
                     echo "RPMs with git hash in NVR are not allowed within releases" >&2
                     exit 1
