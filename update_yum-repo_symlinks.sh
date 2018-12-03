2b39d680324681611e3bcf75d542e1e401538a58
Sandro Bonazzola <sbonazzo@redhat.com>
2018-12-03 08:26:44 +0100

    update_yum-repo_symlinks: add date
    Adding date so logs are more meaningful
    
    Change-Id: I751707e37bdba649dcb6dc4da132e3d4c311c718
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- update_yum-repo_symlinks.sh
+++ update_yum-repo_symlinks.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+date
+
 export MASTER="/var/www/html/pub/ovirt-master-snapshot/rpm/el7/noarch"
 export OVIRT42="/var/www/html/pub/ovirt-4.2-snapshot/rpm/el7/noarch"
 export YUMREPO="/var/www/html/pub/yum-repo"
@@ -7,3 +9,4 @@ export YUMREPO="/var/www/html/pub/yum-repo"
 ln -sf `ls "${MASTER}"/ovirt-release-master-4* |tail -n 1` "${YUMREPO}/ovirt-release-master.rpm"
 ln -sf `ls "${MASTER}"/ovirt-release-master-experimental-4* |tail -n 1` "${YUMREPO}/ovirt-release-master-experimental.rpm"
 ln -sf `ls "${OVIRT42}"/ovirt-release42-snapshot-4* |tail -n 1` "${YUMREPO}/ovirt-release42-snapshot.rpm"
+echo "-------------------------------"
