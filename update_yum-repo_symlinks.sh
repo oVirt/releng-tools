9b2d177fcf1bfb7f02286b1910a6a4d51e79f945
Sandro Bonazzola <sbonazzo@redhat.com>
2019-03-21 07:54:46 +0100

    update_yum-repo_symlinks: drop experimental
    experimental was available in 4.2 and doesn't exist
    anymore on master.
    
    Change-Id: I2b118ff1a2df49efe0b4c3638acd5055cfc2f6c3
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- update_yum-repo_symlinks.sh
+++ update_yum-repo_symlinks.sh
@@ -8,7 +8,6 @@ export OVIRT43="/var/www/html/pub/ovirt-4.3-snapshot/rpm/el7/noarch"
 export YUMREPO="/var/www/html/pub/yum-repo"
 
 ln -sf `ls "${MASTER}"/ovirt-release-master-4* |tail -n 1` "${YUMREPO}/ovirt-release-master.rpm"
-ln -sf `ls "${MASTER}"/ovirt-release-master-experimental-4* |tail -n 1` "${YUMREPO}/ovirt-release-master-experimental.rpm"
 ln -sf `ls "${OVIRT42}"/ovirt-release42-snapshot-4* |tail -n 1` "${YUMREPO}/ovirt-release42-snapshot.rpm"
 ln -sf `ls "${OVIRT43}"/ovirt-release43-snapshot-4* |tail -n 1` "${YUMREPO}/ovirt-release43-snapshot.rpm"
 echo "-------------------------------"
