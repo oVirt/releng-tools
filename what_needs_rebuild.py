cbf902be2cefa949b27df0611e7a1354f9e21406
Sandro Bonazzola <sbonazzo@redhat.com>
2019-05-13 16:50:47 +0200

    script: add what_needs_rebuild
    adding a simple script that query bugzilla for a target milestone
    and prints a list of packages requiring to be built
    referencing bugs associated to it.
    
    Change-Id: Ia4cba4a3fbe912623410f94e7c212bcba970b82a
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

---
+++ what_needs_rebuild.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python2
+
+from release_notes_git import Bugzilla
+import sys
+
+
+def main(milestone):
+    bz = Bugzilla()
+    bug_list = bz.get_bugs_in_milestone(milestone)
+    products = {}
+    for bug in bug_list:
+        if bug.status != 'MODIFIED':
+            # Assuming here that bugs are moved to QE only after a build is
+            # available.
+            continue
+        if (bug.product == 'Red Hat Enterprise Virtualization Manager'):
+            if (bug.component != 'Documentation'):
+                products.setdefault(bug.component, [])
+                products[bug.component].append(bug.id)
+        else:
+            products.setdefault(bug.product, [])
+            products[bug.product].append(bug.id)
+    print("The following products need to be built for {m}".format(
+        m=milestone
+    ))
+    for p in products:
+        print(" - {p}".format(p=p))
+        for v in products[p]:
+            print(" |- https://bugzilla.redhat.com/{v}".format(v=v))
+
+
+if __name__ == '__main__':
+    milestone = sys.argv[1]
+    main(milestone)
