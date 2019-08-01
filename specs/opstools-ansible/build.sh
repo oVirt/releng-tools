fe5448624e28349a95fed00b7776471a57518da9
Sandro Bonazzola <sbonazzo@redhat.com>
2019-08-01 09:43:35 +0200

    cleanup: drop spec files not handled in ovirt
    dropped spec files for packages previously handled in ovirt and
    now included in distributions we support.
    
    Change-Id: I60dde3593290febd9d34dfd3bda8ddf6c2b7f33f
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- specs/opstools-ansible/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -e
-
-spectool -g ./*.spec
-rpmbuild \
-    -bs \
-    --define="_sourcedir ${PWD}" \
-    --define="_srcrpmdir ${PWD}" \
-    --define="_rpmdir ${PWD}" \
-    *.spec
--- specs/sos/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -e
-
-spectool --get-files --sources sos.spec
-rpmbuild \
-    -ba \
-    --define="_sourcedir ${PWD}" \
-    --define="_srcrpmdir ${PWD}" \
-    --define="_rpmdir ${PWD}" \
-    sos.spec
