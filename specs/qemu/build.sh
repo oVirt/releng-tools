49d55b6623685d5173f7aee2baff4706819e9629
Sandro Bonazzola <sbonazzo@redhat.com>
2019-08-02 07:33:41 +0200

    specs: qemu: rebase on qemu-kvm-rhev-2.12.0-18.el7_6.7
    Change-Id: I6816ef7933de07464a8b2adfc169b25614ced1eb
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- specs/qemu/build.sh
+++ specs/qemu/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-src="qemu-kvm-rhev-2.12.0-18.el7_6.5.src.rpm"
+src="qemu-kvm-rhev-2.12.0-18.el7_6.7.src.rpm"
 url="ftp://ftp.redhat.com/pub/redhat/linux/enterprise/7Server/en/RHEV/SRPMS/${src}"
 patches=(
     "0001-qemu-kvm-spec-remove-branding.patch"
