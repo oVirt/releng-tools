e4b4933412bbd6a942be4f4433b03cda3278b843
Sandro Bonazzola <sbonazzo@redhat.com>
2020-04-08 09:32:57 +0200

    automation: refactor
    - drop fc29 job since fc29 is EOL
    - refactor el8 jobs for using sub-stages
    
    Change-Id: I37ce1595ef87b8a1333ba185ade62d9e08a022d1
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

---
+++ automation/milestone-test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -xe
+[[ -d exported-artifacts ]] || mkdir -p exported-artifacts
+[[ -d compose-test ]] || mkdir -p compose-test
+
+if git show --pretty="format:" --name-only | egrep -q "^milestones/.*conf$"; then
+    for config_file in `git show --pretty="format:" --name-only | egrep "^milestones/.*conf$"`
+    do
+        if [[ -e "${config_file}" ]] ; then
+            python3 automation/check-milestone-previous.py ${config_file}
+            release=`basename "${config_file/.conf/}"`
+            ./release_notes_git.py "${release}" > "exported-artifacts/${release}_notes.md"
+        else
+            echo "Skipping ${config_file} since it has been removed."
+        fi
+    done
+fi
