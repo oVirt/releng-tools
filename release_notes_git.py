6f2467ed0de8f2df190a06d2a4f6add7a3a9f919
Lev Veyde <lveyde@redhat.com>
2020-07-13 20:49:01 +0300

    release_notes_git: Fix community link
    Change-Id: Id0722ed204af00324cf97346ba6a27c0dab8d37f
    Signed-off-by: Lev Veyde <lveyde@redhat.com>

--- release_notes_git.py
+++ release_notes_git.py
@@ -94,7 +94,7 @@ CentOS Linux 8.2 (or similar).
 
 {% if release_type %}
 To find out how to interact with oVirt developers and users and ask questions,
-visit our [community page]"(/community/).
+visit our [community page](/community/).
 All issues or bugs should be reported via
 [Red Hat Bugzilla](https://bugzilla.redhat.com/enter_bug.cgi?classification=oVirt).
 The oVirt Project makes no guarantees as to its suitability or usefulness.
