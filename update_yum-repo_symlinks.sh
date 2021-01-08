b05e989b631ae5e9e976e84ff3750de52f216449
Sandro Bonazzola <sbonazzo@redhat.com>
2021-01-08 09:25:36 +0100

    update_yum-repo_symlinks: drop 4.2 snapshot
    4.2 gone EOL long time ago, time to drop the nightly repo for it.
    
    Change-Id: I7abf25402fecb8fc7f96cbde8f168b4e1d9ad651
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- update_yum-repo_symlinks.sh
+++ update_yum-repo_symlinks.sh
@@ -3,11 +3,9 @@
 date
 
 export MASTER="/var/www/html/pub/ovirt-master-snapshot/rpm/el8/noarch"
-export OVIRT42="/var/www/html/pub/ovirt-4.2-snapshot/rpm/el7/noarch"
 export OVIRT43="/var/www/html/pub/ovirt-4.3-snapshot/rpm/el7/noarch"
 export YUMREPO="/var/www/html/pub/yum-repo"
 
 ln -sf `ls "${MASTER}"/ovirt-release-master-4* |tail -n 1` "${YUMREPO}/ovirt-release-master.rpm"
-ln -sf `ls "${OVIRT42}"/ovirt-release42-snapshot-4* |tail -n 1` "${YUMREPO}/ovirt-release42-snapshot.rpm"
 ln -sf `ls "${OVIRT43}"/ovirt-release43-snapshot-4* |tail -n 1` "${YUMREPO}/ovirt-release43-snapshot.rpm"
 echo "-------------------------------"
