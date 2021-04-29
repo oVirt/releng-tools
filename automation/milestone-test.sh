4fe603750e6af901657abb311ac4cf4344c0f17e
Sandro Bonazzola <sbonazzo@redhat.com>
2021-04-29 08:44:02 +0200

    milestone-test: addressed shellcheck issues
    addressed issues reported by shellcheck analyzer.
    
    Change-Id: I88c4fb371d4d597c8468f9b606dd465514b5983e
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>

--- automation/milestone-test.sh
+++ automation/milestone-test.sh
@@ -2,12 +2,12 @@
 [[ -d exported-artifacts ]] || mkdir -p exported-artifacts
 [[ -d compose-test ]] || mkdir -p compose-test
 
-if git show --pretty="format:" --name-only | egrep -q "^milestones/.*conf$"; then
-    for config_file in `git show --pretty="format:" --name-only | egrep "^milestones/.*conf$"`
+if git show --pretty="format:" --name-only | grep -E -q "^milestones/.*conf$"; then
+    for config_file in $(git show --pretty="format:" --name-only | grep -E "^milestones/.*conf$")
     do
         if [[ -e "${config_file}" ]] ; then
-            python3 automation/check-milestone-previous.py ${config_file}
-            release=`basename "${config_file/.conf/}"`
+            python3 automation/check-milestone-previous.py "${config_file}"
+            release=$(basename "${config_file/.conf/}")
             ./release_notes_git.py "${release}" > "exported-artifacts/${release}_notes.md"
         else
             echo "Skipping ${config_file} since it has been removed."
