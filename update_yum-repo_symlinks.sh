5b03c649a4f768265eb4f14bd5bfad8c1643b529
Sandro Bonazzola <sbonazzo@redhat.com>
2021-08-18 12:25:38 +0200

    update_yum-repo_symlinks: add 4.4 support
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>
    Change-Id: I7ae07da70eea55bcbaa6ddf9816fc1899535c17c

--- update_yum-repo_symlinks.sh
+++ update_yum-repo_symlinks.sh
@@ -4,8 +4,10 @@ date
 
 export MASTER="/var/www/html/pub/ovirt-master-snapshot/rpm/el8/noarch"
 export OVIRT43="/var/www/html/pub/ovirt-4.3-snapshot/rpm/el7/noarch"
+export OVIRT44="/var/www/html/pub/ovirt-4.4-snapshot/rpm/el8/noarch"
 export YUMREPO="/var/www/html/pub/yum-repo"
 
-ln -sf `ls "${MASTER}"/ovirt-release-master-4* |tail -n 1` "${YUMREPO}/ovirt-release-master.rpm"
-ln -sf `ls "${OVIRT43}"/ovirt-release43-snapshot-4* |tail -n 1` "${YUMREPO}/ovirt-release43-snapshot.rpm"
+ln -sf "$(ls "${MASTER}"/ovirt-release-master-4* |tail -n 1)" "${YUMREPO}/ovirt-release-master.rpm"
+ln -sf "$(ls "${OVIRT43}"/ovirt-release43-snapshot-4* |tail -n 1)" "${YUMREPO}/ovirt-release43-snapshot.rpm"
+ln -sf "$(ls "${OVIRT44}"/ovirt-release44-snapshot-4* |tail -n 1)" "${YUMREPO}/ovirt-release44-snapshot.rpm"
 echo "-------------------------------"
