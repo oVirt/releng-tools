b39999c957ee693fce3df14b2e71571e2684c54a
Sandro Bonazzola <sbonazzo@redhat.com>
2021-09-14 13:49:59 +0200

    automation: generate notes with --contrib-project-list
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>
    Change-Id: I8e14970af0b6714c9afb41e1c8f534ee4485532e

--- automation/milestone-test.sh
+++ automation/milestone-test.sh
@@ -8,7 +8,7 @@ if git show --pretty="format:" --name-only | grep -E -q "^milestones/.*conf$"; t
         if [[ -e "${config_file}" ]] ; then
             python3 automation/check-milestone-previous.py "${config_file}"
             release=$(basename "${config_file/.conf/}")
-            ./release_notes_git.py "${release}" > "exported-artifacts/${release}_notes.md"
+            ./release_notes_git.py --contrib-project-list "${release}" > "exported-artifacts/${release}_notes.md"
         else
             echo "Skipping ${config_file} since it has been removed."
         fi
