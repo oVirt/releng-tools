b2e7595a14b3a00047cce39b8aff642ad3919249
Sandro Bonazzola <sbonazzo@redhat.com>
2021-09-20 15:58:23 +0200

    update_yum-repo_symlinks.sh: add master tested
    Add symlink to ovirt-release-master-tested
    
    Signed-off-by: Sandro Bonazzola <sbonazzo@redhat.com>
    Change-Id: I8cc734f6eab5ec780cc19aa02b3800dcf9fdc78e

--- update_yum-repo_symlinks.sh
+++ update_yum-repo_symlinks.sh
@@ -7,5 +7,6 @@ export OVIRT44="/var/www/html/pub/ovirt-4.4-snapshot/rpm/el8/noarch"
 export YUMREPO="/var/www/html/pub/yum-repo"
 
 ln -sf "$(ls "${MASTER}"/ovirt-release-master-4* |tail -n 1)" "${YUMREPO}/ovirt-release-master.rpm"
+ln -sf "$(ls "${MASTER}"/ovirt-release-master-tested-4* |tail -n 1)" "${YUMREPO}/ovirt-release-master-tested.rpm"
 ln -sf "$(ls "${OVIRT44}"/ovirt-release44-snapshot-4* |tail -n 1)" "${YUMREPO}/ovirt-release44-snapshot.rpm"
 echo "-------------------------------"
